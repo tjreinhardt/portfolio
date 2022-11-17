69b7d996836e4be26c803cb30e9de660e8628487
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-11-16 21:22:40 -0800

    need to fix experience sanity queries

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { PageInfo } from "../typings"
+import { urlFor } from '../sanity'
 
-type Props = {}
+type Props = {
+  pageInfo: PageInfo
+}
 
-function About({ }: Props) {
+function About({ pageInfo }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -25,7 +29,7 @@ function About({ }: Props) {
         }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        src='http://partyhopper.s3.amazonaws.com/7e8ecf3c5c5045e7800056ca55f43cad.jpg'
+        src={urlFor(pageInfo?.profilePic).url()}
         className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
       />
 
@@ -33,8 +37,8 @@ function About({ }: Props) {
         <h4 className='text-4xl font-semibold'>Here is a{" "}
           <span className='underline decoration-[#F7AB0A]'>little</span> background
         </h4>
-        <p className='text-sm'>
-          I'm Tim. I've been deep-diving into software engineering for the past year. Previously, I was an audio engineer, working in recording studios and live-media productions across the United States. Being a self-taught musician as well, I've always had a passion for creation. Recently I graduated from AppAcademy, and have been creating applications that challenge me as a developer and am constantly implementing the latest technologies. Outside of programming I'm either adventuring with my brother & dog, gaming with my friends, or even helping my friends optimise their own business applications with my newly acquired skills.
+        <p className='text-md'>
+          {pageInfo?.backgroundInformation}
         </p>
       </div>
 
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Experience } from '../typings'
+import { urlFor } from '../sanity';
+import { fetchExperiences } from '../utils/fetchExperiences';
 
-type Props = {}
+type Props = {
+  experience: Experience;
+}
 
-function ExperienceCard({ }: Props) {
+function ExperienceCard({ experience }: Props) {
   return (
     <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
       <motion.img
@@ -15,18 +20,21 @@ function ExperienceCard({ }: Props) {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         className='w-32 h-32 rounded-full md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
-        src="http://partyhopper.s3.amazonaws.com/2e561f860c024c7c99717bf054efaf76.png"
+        src={urlFor(experience?.companyImage).url()}
+        alt=""
       />
 
       <div className='px-0 md:px-10'>
         <h4 className='text-4xl font-light'>FullStack Engineer</h4>
         <p className='font-bold text-2xl mt-1'> Name </p>
         <div className='flex space-x-2 my-2'>
-          <img
-            className='h-10 w-10 rounded-full'
-            src="https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png"
-            alt=""
-          />
+          {experience.technologies.map((technology) => (
+            <img
+              key={technology._id}
+              className="h-10 w-10 rounded-full"
+              src={urlFor(technology.image).url()}
+            />
+          ))}
           <img
             className='h-10 w-10 rounded-full'
             src="https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483c1a8174556463-500x500.png"
@@ -39,13 +47,13 @@ function ExperienceCard({ }: Props) {
           />
         </div>
         <p className='uppercase py-5 text-gray-300'>Started work... - Ended...</p>
-
+        {new Date(experience.dateStarted).toDateString()} - {experience.isCurrentlyWorkingHere ?
+          "Present"
+          : new Date(experience.dateEnded).toDateString()}
         <ul className='list-disc space-y-4 ml-5 text-lg'>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
-          <li>Summary Points</li>
+          {experience.points.map((point, i) => (
+            <li key={i}>{point}</li>
+          ))}
         </ul>
 
       </div>
