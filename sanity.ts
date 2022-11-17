7d086399a4af5b1b65d0cb6100687f0309bc0d5d
Tim Reinhardt <timreinhardtmusic@gmail.com>
2022-11-17 13:03:45 -0800

    finished

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATABASE || "production",
-  projectId: '7l1rv7iu',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 }
